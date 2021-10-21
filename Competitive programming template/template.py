a9a12837a408badb585d5cca23ed85d8b5f9b413
Meyank M <65050847+memeyankm@users.noreply.github.com>
2021-10-21 23:49:00 +0530

    Create template.py

---
+++ Competitive programming template/template.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import math
+
+def function():
+    '''
+    Enter code here
+    '''
+   
+if "__name__" == "__main__":
+    input_val = list(map(int, input().split()))
+    output_val = function(input_val)
+    print(output_val)
+    
